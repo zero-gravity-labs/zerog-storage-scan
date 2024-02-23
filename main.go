cf29ef8669abadfa318cf7822f5ccd8066dc5b9f
boqiu <82121246@qq.com>
2024-02-23 15:57:26 +0800

    remove duplicate init

--- main.go
+++ main.go
@@ -2,12 +2,10 @@ package main
 
 import (
 	"github.com/Conflux-Chain/go-conflux-util/config"
-	"github.com/Conflux-Chain/go-conflux-util/log"
 	"github.com/zero-gravity-labs/zerog-storage-scan/cmd"
 )
 
 func main() {
 	config.MustInit("zerog_storage")
-	log.MustInitFromViper()
 	cmd.Execute()
 }
